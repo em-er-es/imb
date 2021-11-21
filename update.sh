0483579ff1a1fc8a7ccc4b0c1d0988d941df5daa
Skrzypczyk Ernest <emeres.code@onet.eu>
2021-11-21 14:47:53 +0100

    Fix general update script
    Rewrite logic
    
    Remove redundant entries

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-SCRIPTS=(build tests coverage profiling documentation cleanup tests)
-for SCRIPT in "scripts/${SCRIPTS[@]}"; do
-	SCRIPT="${SCRIPT}.sh"
-	[[ -x "${SCRIPT}" ]] && "${SCRIPT}"
+SCRIPTS=(scripts build coverage profiling documentation tests cleanup)
+for SCRIPT in "${SCRIPTS[@]}"; do
+	if [[ "${SCRIPT}" == "${SCRIPTS[0]}" ]]; then
+		continue
+	fi
+	SCRIPT="${SCRIPTS[0]}/${SCRIPT}.sh"
+	echo "Checking ${SCRIPT}"
+	if [[ -x "${SCRIPT}" ]]; then
+		echo "Running ${SCRIPT}"
+		bash "${SCRIPT}"
+	fi
 done
